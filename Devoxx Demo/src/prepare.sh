a0cccc54c95848ded6c8e574bccc75015c04f4f8
mathias kluba <mathias.kluba@gmail.com>
2017-02-26 10:12:08 +0100

    new version of the demo

---
+++ Devoxx Demo/src/prepare.sh
@@ -0,0 +1,4 @@
+sudo ln -s /home/mathias/notebooks/dashboard/ /var/www/html/dashboard
+chmod o+x /home/mathias
+chmod o+x /home/mathias/notebooks/
+chmod -R o+rx /home/mathias/notebooks/dashboard/
---
+++ Devoxx Demo/src/start.sh
@@ -0,0 +1,2 @@
+export JUPYTER_DATA_DIR=/home/mathias/notebooks/data/
+/opt/anaconda2/bin/jupyter-notebook --ip=192.168.1.101 --no-browser -y --notebook-dir=./notebooks
