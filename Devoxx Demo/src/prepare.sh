0732afe8043d7b29c0cc97b9a9b0f7e66c79e8cb
mathias kluba <mathias.kluba@gmail.com>
2017-04-02 15:28:27 +0200

    maj de la démo

--- Devoxx Demo/src/prepare.sh
+++ Devoxx Demo/src/prepare.sh
@@ -2,3 +2,6 @@ sudo ln -s /home/mathias/notebooks/dashboard/ /var/www/html/dashboard
 chmod o+x /home/mathias
 chmod o+x /home/mathias/notebooks/
 chmod -R o+rx /home/mathias/notebooks/dashboard/
+
+/opt/anaconda2/bin/pip install --user git+https://github.com/dunovank/jupyter-themes.git
+~/.local/bin/jt -t chesterish -cellw 1300 -lineh 170 -f roboto -fs 16 -tf roboto -tfs 16
--- Devoxx Demo/src/start.sh
+++ Devoxx Demo/src/start.sh
@@ -1,2 +1,2 @@
 export JUPYTER_DATA_DIR=/home/mathias/notebooks/data/
-/opt/anaconda2/bin/jupyter-notebook --ip=192.168.1.101 --no-browser -y --notebook-dir=./notebooks
+/opt/anaconda2/bin/jupyter-notebook --ip=192.168.2.103 --no-browser -y --notebook-dir=./notebooks
